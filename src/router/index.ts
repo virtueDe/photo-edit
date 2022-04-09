5372977d0f2b9338ab8a38947f66c7d40b4dd982
xiaoling <xiaoling@laihua.com>
2022-04-09 12:42:37 +0800

    chore: init project

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/HomeView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
