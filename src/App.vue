75a539bfa264ce379bdd64bf335a83c8d2ff2eda
ling.xiao <xiaoling@laihua.com>
2022-04-12 18:02:55 +0800

    feat: init layout

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,22 @@
 </template>
 
 <style lang="less">
+* {
+  box-sizing: border-box;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,34 @@
 <template>
-  <div class="home">
-    {{ isTest }}
+  <div class="edit">
+    <div class="edit-toolbar"></div>
+    <div class="edit-canvas"></div>
+    <div class="edit-controls"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue"
-const isTest = ref<boolean>(false)
+// import { ref } from "vue"
+// const isTest = ref<boolean>(false)
 </script>
+
+<style scoped lang="less">
+.edit {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  &-toolbar {
+    height: 50px;
+    background-color: #262626;
+  }
+  &-canvas {
+    flex: 1;
+    background-color: #121212;
+  }
+  &-controls {
+    height: 120px;
+    background-color: #262626;
+  }
+}
+</style>
