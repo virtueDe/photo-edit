2f5e8998354a6886a176685c8717828f1aaf68a8
xiaoling <xiaoling@laihua.com>
2022-07-14 15:41:38 +0800

    fix: 行尾序列

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 
-createApp(App).use(router).mount("#app")
+const app = createApp(App).use(router).mount("#app")
+
+console.log(111, app)
